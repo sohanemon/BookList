ce120ced389865ed08f753e3cbd4998986b78453
sohanemon <m.sohan.rse@gmail.com>
2021-11-20 11:26:59 +0600

    form ok alpha

--- js/script.js
+++ js/script.js
@@ -10,10 +10,31 @@ class Book {
   }
 }
 
+class UI {
+  constructor() {}
+  addToTable(book) {
+    let table = document.querySelector("#book-list");
+    let row = document.createElement("tr");
+    row.innerHTML = `<td>${book.title}</><td>${book.author}</><td>${book.isbn}</><td><a href="#" class="delete">x</>`;
+    table.appendChild(row);
+  }
+  clearField(book) {
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#isbn").value = "";
+  }
+}
 // Events
 form.addEventListener("submit", addNewBook);
 
 // Function
 function addNewBook(e) {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let isbn = document.querySelector("#isbn").value;
+  let book = new Book(title, author, isbn);
+  let ui = new UI();
+  ui.addToTable(book);
+  ui.clearField(book);
   e.preventDefault();
 }
